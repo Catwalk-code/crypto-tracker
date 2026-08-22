8916e2e9fa8a78c2b0950d38cec0d84ed9898bb4
Egor Orlov <egorr5302@gmail.com>
2026-08-22 22:00:39 +0300

    Added function for getting current price of coin, Binance public api is used, function isn't used yet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi import HTTPException
+import httpx
 
 """Model for creating of active (without id, currnet price)"""
 class HoldingCreate(BaseModel):
@@ -62,6 +63,27 @@ def delete_holding(holding_id: int):
     # If not found        
     raise HTTPException(status_code=404, detail="Holding not found")
 
+async def get_current_price(coin:str) -> float:
+    """Getting current price of coin using Binance API"""
+    
+    #Binance use symols like BTCUSDT, ETHUSDT
+    symbol = f"{coin}USDT"
+
+    url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}"
+
+    '''I found that creating a user using a fuction is a bad idea, the app 
+    will work faster in case if user is created in the start of server. This should be rewrite using lifespan'''
+    async with httpx.AsyncCLient() as client:
+        response = await client.get(url)
+
+        #If not found
+        if response.status_code != 200:
+            print(f"Error when receiving the price {coin}: {response.status_code}")
+            return 0
+        
+        data = response.json()
+        price = float(data["price"])
+        return price
 
 @app.get("/")
 def root():
