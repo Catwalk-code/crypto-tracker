1fe17672d41c46f275a097a0d729f67d0d0a5a10
Egor Orlov <egorr5302@gmail.com>
2026-08-25 17:59:06 +0300

    DB works now

--- database.py
+++ database.py
@@ -1,15 +1,25 @@
 import asyncpg
 import os
 from dotenv import load_dotenv
+from decimal import Decimal
+from services import get_current_price
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+def convert_decimal_to_float(data: dict) -> dict:
+    return {
+        key: float(value) if isinstance(value, Decimal) else value
+        for key, value in data.items()
+    }
+
 
 async def init_db():
     """Create tables when start server"""
     conn = await asyncpg.connect(DATABASE_URL)
 
-    await conn.execite("""
+    await conn.execute("""
         CREATE TABLE IF NOT EXISTS holdings (
             id SERIAL PRIMARY KEY,
             coin VARCHAR(10) NOT NULL,
@@ -22,20 +32,26 @@ async def init_db():
     await conn.close()
     print("Database initialized")
 
-async def get_all_holdings() -> list[dict]:
+async def get_holdings() -> list[dict]:
     conn = await asyncpg.connect(DATABASE_URL)
-    rows = await conn.fetch("SELECT * FROM holdings ORDER BY id")
+    row = await conn.fetch("SELECT * FROM holdings ORDER BY id")
     await conn.close()
-    return [dict(row) for row in rows]
+    return [convert_decimal_to_float(dict(r.items())) for r in row]
 
 async def get_holding_by_id(holding_id: int) -> dict | None:
     conn = await asyncpg.connect(DATABASE_URL)
-    row = await asyncpg.fetchrow("SELECT * FROM holdings WHERE id = $1", holding_id)
+    row = await conn.fetchrow("SELECT * FROM holdings WHERE id = $1", holding_id)
     await conn.close()
-    return dict(row) if row else None
+    return convert_decimal_to_float(dict(row.items()))
 
 async def create_holding(coin: str, amount: float, buy_price:float) -> dict:
     conn = await asyncpg.connect(DATABASE_URL)
+    
+    price = await get_current_price(coin)
+    if price is None:
+        await conn.close()
+        raise HTTPException(status_code=404, detail="Coin not found on Binance")
+
     row = await conn.fetchrow(
         """
         INSERT INTO holdings (coin, amount, buy_price)
@@ -45,7 +61,14 @@ async def create_holding(coin: str, amount: float, buy_price:float) -> dict:
         coin, amount, buy_price
     )
     await conn.close()
-    return dict(row)
+
+    result = convert_decimal_to_float(dict(row.items()))
+    result["current_price"] = price
+    invested = result["amount"] * result["buy_price"]
+    current_value = result["amount"] * price
+    result["profit_loss"] = current_value - invested
+
+    return result
 
 async def delete_holding(holding_id: int) -> bool:
     conn = await asyncpg.connect(DATABASE_URL)
--- main.py
+++ main.py
@@ -3,82 +3,94 @@ from fastapi import HTTPException
 
 from models import  Holding, HoldingCreate, PortfolioResponse
 from services import get_current_price
+from database import init_db, get_holdings, get_holding_by_id, create_holding, delete_holding
 
-holdings = []
-next_id = 1
+
 app = FastAPI()
 
 
-@app.post("/holdings", response_model=Holding)
-async def create_holding(holding: HoldingCreate):
-    '''Using global value is not a good practice. Later it will be replaced 
-    with PostgreSQL'''
-    # We say to Python use value that has been created 
-    # globally (not in function)
-    global next_id 
+@app.on_event("startup")
+async def startup():
+    await init_db()
 
+
+@app.post("/holdings", response_model=Holding)
+async def create_holding_endpoint(holding: HoldingCreate):
     price = await get_current_price(holding.coin)
 
     #If price = None, than coin doesn't exist
     if price is None:
         raise HTTPException(status_code=400, detail="Coin not found")
 
-    # Copy all fields from holding to the dictionary
-    data = holding.model_dump()
-    
-    #Add id
-    data["id"] = next_id
-    next_id += 1
+    new_holding = await create_holding(
+        coin=holding.coin,
+        amount=holding.amount,
+        buy_price=holding.buy_price
+    )
 
-    #Create Holding from dictionary
-    new_holding = Holding(**data)
-    holdings.append(new_holding)
+    new_holding["current_price"] = price
+    invested = new_holding["amount"] * new_holding["buy_price"]
+    current_value = new_holding["amount"] * price
+    new_holding["profit_loss"] = current_value - invested
 
     return new_holding
 
 
 @app.get("/holdings", response_model=list[Holding])
-async def get_holdings():
+async def get_holdings_endpoint():
+    holdings = await get_holdings()
+
     for h in holdings:
-        current_price = await get_current_price(h.coin)
+        current_price = await get_current_price(h["coin"])
 
         #If None, than coin is not found
         if current_price is None:
-            h.currnet_price = 0.0 #set 0 so as not to fall
-            h.profit_loss = 0.0
+            h["currnet_price"] = 0.0 #set 0 so as not to fall
+            h["profit_loss"] = 0.0
             continue #Skip this coin add go to the next
 
-        h.currnet_price = current_price
-        invested = h.amount * h.buy_price
-        current_value= h.amount * current_price
-        h.profit_loss = current_value - invested
+        h["currnet_price"] = current_price
+        invested = h["amount"] * h["buy_price"]
+        current_value= h["amount"] * current_price
+        h["profit_loss"] = current_value - invested
 
     return holdings
 
 
 @app.get("/holdings/{holding_id}", response_model=Holding)
-def get_holding(holding_id: int):
-    for h in holdings:
-        if h.id == holding_id:
-            return h
+async def get_holding_by_id_endpoint(holding_id: int):
+    holding = await get_holding_by_id(holding_id)
 
-    raise HTTPException(status_code=404, detail="holding not found")
+    if not holding:
+        raise HTTPException(status_code=404, detail="Holding not found")
 
+    current_price = await get_current_price(holding["coin"])
+    if current_price is not None:
+        holding["current_price"] = current_price
+        invested = holding["amount"] * holding["buy_price"]
+        current_value = holding["amount"] * current_price
+        holding["profit_loss"] = current_value - invested
+    else:
+        holding["current_price"] = 0.0
+        holding["profit_loss"] = 0.0
+
+    return holding
+    
 
 @app.delete("/holdings/{holding_id}")
-def delete_holding(holding_id: int):
-    global holdings
-    for i, h in enumerate(holdings):
-        if h.id == holding_id:
-            holdings.pop(i)
-            return {"message": f"Holding {holding_id} deleted"}
+async def delete_holding_endpoint(holding_id: int):
+    deleted = await delete_holding(holding_id)
 
-    # If not found        
-    raise HTTPException(status_code=404, detail="Holding not found")
+    if not deleted:
+        raise HTTPException(status_code=404, detail="Holding not found")
+
+    return {"message": f"Holding {holding_id} deleted"}
 
 
 @app.get("/portfolio", response_model=PortfolioResponse)
 async def get_portfolio():
+    holdings = await get_holdings()
+
     if not holdings:
         return PortfolioResponse(
             total_value=0.0,
@@ -94,13 +106,19 @@ async def get_portfolio():
 
     #Update prices for all actives
     for h in holdings:
-        current_price = await get_current_price(h.coin)
-        h.currnet_price = current_price
+        current_price = await get_current_price(h["coin"])
+        
+        if current_price is None:
+            h["current_price"] = 0.0
+            h["profit_loss"] = 0.0
+            continue
+        
+        h["currnet_price"] = current_price
 
-        invested = h.amount * h.buy_price
-        current_value = h.amount * current_price
+        invested = h["amount"] * h["buy_price"]
+        current_value = h["amount"] * current_price
 
-        h.profit_loss = current_value - invested
+        h["profit_loss"] = current_value - invested
 
         total_value+= current_value
         total_invested+=invested
